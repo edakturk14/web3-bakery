92b0c0102994744de63ba1fbe679990c6b846d17
Eda Akturk <edaakturk@Edas-MacBook-Pro.local>
2024-06-13 09:57:35 +0300

    Add smart wallet

---
+++ src/wagmi.ts
@@ -0,0 +1,24 @@
+import { http, createConfig } from "wagmi";
+import { baseSepolia } from "wagmi/chains";
+import { coinbaseWallet } from "wagmi/connectors";
+
+export const config = createConfig({
+    chains: [baseSepolia],
+    multiInjectedProviderDiscovery: false,
+    connectors: [
+        coinbaseWallet({
+            appName: "Wagmi Smart Wallet",
+            preference: "smartWalletOnly",
+        }),
+    ],
+    ssr: true,
+    transports: {
+        [baseSepolia.id]: http(),
+    },
+});
+
+declare module "wagmi" {
+    interface Register {
+        config: typeof config;
+    }
+}
